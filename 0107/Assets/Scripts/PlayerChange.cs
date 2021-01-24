078f7a17d04798e9d4788185ab878643085e2e8c
RAY <horixkashima1128@gmail.com>
2021-01-24 21:39:00 +0800

    可以切換角色，但是n_player只有idle動畫

--- 0107/Assets/Scripts/PlayerChange.cs
+++ 0107/Assets/Scripts/PlayerChange.cs
@@ -9,22 +9,32 @@ public class PlayerChange : MonoBehaviour
     private int form_index;
     void Start()
     {
-        Maz_form[0] = gameObject.transform.GetChild(0).gameObject;
-        Maz_form[1] = gameObject.transform.GetChild(1).gameObject;
+        Maz_form[0] = this.transform.GetChild(0).gameObject;
+        Maz_form[1] = this.transform.GetChild(1).gameObject;
+
+        Maz_form[0].SetActive(true);
+        Maz_form[1].SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            form_index = PlayerController.Maz_ID_Now;
+            form_index = PlayerController.Maz_ID_Now; 
+
             if (forkTrigger.getFork)
             {
                 form_index = 1;
+                Maz_form[0].SetActive(false);
+                Maz_form[1].SetActive(true);
             }
         }
-        ChangeFinish();
+
+        
+        
     }
+
+    
 
     void ChangeFinish()//將原本型態子物件關閉後，開啟新型態的子物件，並更新現在型態的ID
     {
@@ -33,4 +43,5 @@ public class PlayerChange : MonoBehaviour
 
         PlayerController.Maz_ID_Now = form_index;
     }
+
 }
